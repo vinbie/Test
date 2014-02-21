0d757137fdba79a23e2801d14138a14d90d4bc65
vinbie <vinbie@gmail.com>
2014-02-22 01:09:47 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,3 @@
-
 public class HelloWorld {
 
 	/**
@@ -6,7 +5,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world vinoth");
 	}
 
 }
